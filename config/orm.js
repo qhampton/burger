46dda6e5f3738c1269645420db7f2864935fcdd0
Quinlan <qnhampton93@gmail.com>
2019-09-29 12:51:11 -0700

    updated orm logic in config folder to have a selectAll, insertOne, and updateOne

--- config/orm.js
+++ config/orm.js
@@ -2,6 +2,28 @@
 var connection = require("../config/connection.js");
 
 //ORM: selectAll, insertOne, updateOne
-
+var orm = {
+    selectAll: function (tableName, cb) {
+        var query = "SELECT * FROM ??";
+        connection.query(query, [tableName], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    insertOne: function (tableName, columnName, value, cb) {
+        var query = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(query, [tableName, columnName, value], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    },
+    updateOne: function (tableName, columnName, newVal, itemId, cb) {
+        var query = "UPDATE ?? SET ?? == ? WHERE id=?";
+        connection.query(query, [tableName, columnName, newVal, itemId], function (err, result) {
+            if (err) throw err;
+            cb(result);
+        })
+    }
+}
 // Export the orm object for the model 
 module.exports = orm;
