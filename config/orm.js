8387da2b5f22a1d80902dbfe228d810d6fbb94a5
Quinlan <qnhampton93@gmail.com>
2019-09-28 23:51:07 -0700

    added database password into hidden file, completed configuration connection file

---
+++ config/orm.js
@@ -0,0 +1,7 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+//ORM: selectAll, insertOne, updateOne
+
+// Export the orm object for the model 
+module.exports = orm;
